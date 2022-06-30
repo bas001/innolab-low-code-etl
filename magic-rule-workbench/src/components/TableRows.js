ba68bac35e5b1660dc19425030deb87f83503ec9
bseifert <bastianseifert09@aol.de>
2022-06-30 10:22:38 +0200

    schoenheit

--- magic-rule-workbench/src/components/TableRows.js
+++ magic-rule-workbench/src/components/TableRows.js
@@ -5,8 +5,10 @@ function TableRows({rowsData, deleteTableRows, handleChange}) {
         switch (status) {
             case 'green':
                 return '#d2fac3'
-            case 'red':
+            case 'yellow':
                 return '#faf8b6'
+            case 'red':
+                return '#ff7177'
             default:
                 return '#f5f4ed'
         }
@@ -20,17 +22,17 @@ function TableRows({rowsData, deleteTableRows, handleChange}) {
 
                 <tr key={index}>
                     <td><textarea value={input1} onChange={(evnt) => (handleChange(index, evnt))} name="input1"
-                                  className="form-control"/></td>
+                                  className="form-control" style={{resize: "both"}}/></td>
                     <td><textarea value={input2} onChange={(evnt) => (handleChange(index, evnt))} name="input2"
-                                  className="form-control"/></td>
+                                  className="form-control" style={{resize: "both"}}/></td>
                     <td><textarea value={input3} onChange={(evnt) => (handleChange(index, evnt))} name="input3"
-                                  className="form-control"/></td>
+                                  className="form-control" style={{resize: "both"}}/></td>
                     <td><textarea value={input4} onChange={(evnt) => (handleChange(index, evnt))} name="input4"
-                                  className="form-control"/></td>
+                                  className="form-control" style={{resize: "both"}}/></td>
                     <td><textarea value={expectedOutput} onChange={(evnt) => (handleChange(index, evnt))} name="expectedOutput"
-                                  className="form-control"/></td>
+                                  className="form-control" style={{resize: "both"}}/></td>
                     <td><textarea value={actualOutput} onChange={(evnt) => (handleChange(index, evnt))} name="actualOutput"
-                                  disabled={true} style={{backgroundColor: cssColor(status)}} className="form-control"/></td>
+                                  disabled={true} style={{backgroundColor: cssColor(status), resize:"both"}} className="form-control"/></td>
                     <td>
                         <button className="btn btn-outline-danger" onClick={() => (deleteTableRows(index))}>x</button>
                     </td>
